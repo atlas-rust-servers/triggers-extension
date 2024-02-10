cf35a6151621c5252970a1e5119ffad67bb18ab2
Ilovepatatos <gabriel@khome.org>
2024-02-10 00:35:07 -0500

    Create IPlayerTrigger.cs

---
+++ src/TriggersExt/Volumes/IPlayerTrigger.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Oxide.Ext.TriggersExt;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public interface IPlayerTrigger
+{
+    Vector3 Position { get; set; }
+    Quaternion Rotation { get; set; }
+
+    event Action<BasePlayer> OnPlayerWalksIn, OnPlayerWalksOut;
+
+    void Kill();
+}
