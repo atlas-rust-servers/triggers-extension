b1e7127ce9d11c7fabe7e3b9a174f00d24aa89ed
Ilovepatatos <gabriel@khome.org>
2024-02-10 00:34:41 -0500

    Create TriggersExt.cs

---
+++ src/TriggersExt/TriggersExt.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using Oxide.Core;
+using Oxide.Core.Extensions;
+
+namespace Oxide.Ext.TriggersExt;
+
+[UsedImplicitly]
+public class TriggersExt : Extension
+{
+    private static readonly VersionNumber s_ExtensionVersion = new(1, 0, 0);
+
+    public override string Name => "TriggersExt";
+    public override string Author => "Ilovepatatos";
+    public override VersionNumber Version => s_ExtensionVersion;
+
+    public override bool SupportsReloading => true;
+
+    public TriggersExt(ExtensionManager manager) : base(manager) { }
+}
